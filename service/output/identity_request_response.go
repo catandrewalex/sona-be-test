c9d764129e59b9b881cbdc217a6a85f5f3bef04a
FerdiantJoshua <ferdiant.98@gmail.com>
2023-05-21 21:36:45 +0700

    Restructure identity_request_response.go

--- service/output/identity_request_response.go
+++ service/output/identity_request_response.go
@@ -10,50 +10,6 @@ const (
 	MaxResultsPerPage_GetUsers = Default_MaxResultsPerPage
 )
 
-type GetUsersRequest struct {
-	PaginationRequest
-}
-type GetUsersResponse struct {
-	Data    GetUsersResult `json:"data"`
-	Message string         `json:"message,omitempty"`
-}
-type GetUsersResult struct {
-	Results []identity.User `json:"results"`
-	PaginationResponse
-}
-
-func (r GetUsersRequest) Validate() errs.ValidationError {
-	errorDetail := make(errs.ValidationErrorDetail, 0)
-	if validationErr := r.PaginationRequest.Validate(MaxPage_GetUsers, MaxResultsPerPage_GetUsers); validationErr != nil {
-		errorDetail = validationErr.GetErrorDetail()
-	}
-
-	if len(errorDetail) > 0 {
-		return errs.NewValidationError(errs.ErrInvalidRequest, errorDetail)
-	}
-	return nil
-}
-
-type GetUserRequest struct {
-	ID identity.UserID `json:"-"` // we exclude the JSON tag as we'll populate the ID from URL param (not from JSON body or URL query param)
-}
-type GetUserResponse struct {
-	Data    identity.User `json:"data"`
-	Message string        `json:"message,omitempty"`
-}
-
-func (r GetUserRequest) Validate() errs.ValidationError {
-	errorDetail := make(errs.ValidationErrorDetail, 0)
-	if r.ID == identity.UserID_None {
-		errorDetail["id"] = "id cannot be empty"
-	}
-
-	if len(errorDetail) > 0 {
-		return errs.NewValidationError(errs.ErrInvalidRequest, errorDetail)
-	}
-	return nil
-}
-
 type SignUpRequest struct {
 	Email      string              `json:"email"`
 	Password   string              `json:"password"`
@@ -153,5 +109,49 @@ func (r ResetPasswordRequest) Validate() errs.ValidationError {
 		return errs.NewValidationError(errs.ErrInvalidRequest, errorDetail)
 	}
 
+	return nil
+}
+
+type GetUsersRequest struct {
+	PaginationRequest
+}
+type GetUsersResponse struct {
+	Data    GetUsersResult `json:"data"`
+	Message string         `json:"message,omitempty"`
+}
+type GetUsersResult struct {
+	Results []identity.User `json:"results"`
+	PaginationResponse
+}
+
+func (r GetUsersRequest) Validate() errs.ValidationError {
+	errorDetail := make(errs.ValidationErrorDetail, 0)
+	if validationErr := r.PaginationRequest.Validate(MaxPage_GetUsers, MaxResultsPerPage_GetUsers); validationErr != nil {
+		errorDetail = validationErr.GetErrorDetail()
+	}
+
+	if len(errorDetail) > 0 {
+		return errs.NewValidationError(errs.ErrInvalidRequest, errorDetail)
+	}
+	return nil
+}
+
+type GetUserRequest struct {
+	ID identity.UserID `json:"-"` // we exclude the JSON tag as we'll populate the ID from URL param (not from JSON body or URL query param)
+}
+type GetUserResponse struct {
+	Data    identity.User `json:"data"`
+	Message string        `json:"message,omitempty"`
+}
+
+func (r GetUserRequest) Validate() errs.ValidationError {
+	errorDetail := make(errs.ValidationErrorDetail, 0)
+	if r.ID == identity.UserID_None {
+		errorDetail["id"] = "id cannot be empty"
+	}
+
+	if len(errorDetail) > 0 {
+		return errs.NewValidationError(errs.ErrInvalidRequest, errorDetail)
+	}
 	return nil
 }
