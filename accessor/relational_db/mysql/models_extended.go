25afd8d73ad06c1769fb29f9babee7305841b3de
FerdiantJoshua <ferdiant.98@gmail.com>
2023-06-06 18:47:47 +0700

    Simplify models_extended.go

--- accessor/relational_db/mysql/models_extended.go
+++ accessor/relational_db/mysql/models_extended.go
@@ -2,110 +2,34 @@ package mysql
 
 // ============================== TEACHER ==============================
 func (r GetTeacherByIdRow) ToGetTeachersRow() GetTeachersRow {
-	return GetTeachersRow{
-		ID:            r.ID,
-		UserID:        r.UserID,
-		Username:      r.Username,
-		Email:         r.Email,
-		UserDetail:    r.UserDetail,
-		PrivilegeType: r.PrivilegeType,
-		IsDeactivated: r.IsDeactivated,
-		CreatedAt:     r.CreatedAt,
-	}
+	return GetTeachersRow(r)
 }
 func (r GetTeachersByIdsRow) ToGetTeachersRow() GetTeachersRow {
-	return GetTeachersRow{
-		ID:            r.ID,
-		UserID:        r.UserID,
-		Username:      r.Username,
-		Email:         r.Email,
-		UserDetail:    r.UserDetail,
-		PrivilegeType: r.PrivilegeType,
-		IsDeactivated: r.IsDeactivated,
-		CreatedAt:     r.CreatedAt,
-	}
+	return GetTeachersRow(r)
 }
 
 // ============================== STUDENT ==============================
 func (r GetStudentByIdRow) ToGetStudentsRow() GetStudentsRow {
-	return GetStudentsRow{
-		ID:            r.ID,
-		UserID:        r.UserID,
-		Username:      r.Username,
-		Email:         r.Email,
-		UserDetail:    r.UserDetail,
-		PrivilegeType: r.PrivilegeType,
-		IsDeactivated: r.IsDeactivated,
-		CreatedAt:     r.CreatedAt,
-	}
+	return GetStudentsRow(r)
 }
 func (r GetStudentsByIdsRow) ToGetStudentsRow() GetStudentsRow {
-	return GetStudentsRow{
-		ID:            r.ID,
-		UserID:        r.UserID,
-		Username:      r.Username,
-		Email:         r.Email,
-		UserDetail:    r.UserDetail,
-		PrivilegeType: r.PrivilegeType,
-		IsDeactivated: r.IsDeactivated,
-		CreatedAt:     r.CreatedAt,
-	}
+	return GetStudentsRow(r)
 }
 
 // ============================== COURSE ==============================
 
 func (r GetCourseByIdRow) ToGetCoursesRow() GetCoursesRow {
-	return GetCoursesRow{
-		CourseID:              r.CourseID,
-		CourseName:            r.CourseName,
-		DefaultFee:            r.DefaultFee,
-		DefaultDurationMinute: r.DefaultDurationMinute,
-	}
+	return GetCoursesRow(r)
 }
 func (r GetCoursesByIdsRow) ToGetCoursesRow() GetCoursesRow {
-	return GetCoursesRow{
-		CourseID:              r.CourseID,
-		CourseName:            r.CourseName,
-		DefaultFee:            r.DefaultFee,
-		DefaultDurationMinute: r.DefaultDurationMinute,
-	}
+	return GetCoursesRow(r)
 }
 
 // ============================== CLASS ==============================
 
 func (r GetClassByIdRow) ToGetClassesRow() GetClassesRow {
-	return GetClassesRow{
-		ClassID:               r.ClassID,
-		TransportFee:          r.TransportFee,
-		IsDeactivated:         r.IsDeactivated,
-		CourseID:              r.CourseID,
-		TeacherID:             r.TeacherID,
-		StudentID:             r.StudentID,
-		EnrollmentID:          r.EnrollmentID,
-		TeacherUsername:       r.TeacherUsername,
-		TeacherDetail:         r.TeacherDetail,
-		CourseName:            r.CourseName,
-		StudentUsername:       r.StudentUsername,
-		StudentDetail:         r.StudentDetail,
-		DefaultFee:            r.DefaultFee,
-		DefaultDurationMinute: r.DefaultDurationMinute,
-	}
+	return GetClassesRow(r)
 }
 func (r GetClassesByIdsRow) ToGetClassesRow() GetClassesRow {
-	return GetClassesRow{
-		ClassID:               r.ClassID,
-		TransportFee:          r.TransportFee,
-		IsDeactivated:         r.IsDeactivated,
-		CourseID:              r.CourseID,
-		TeacherID:             r.TeacherID,
-		StudentID:             r.StudentID,
-		EnrollmentID:          r.EnrollmentID,
-		TeacherUsername:       r.TeacherUsername,
-		TeacherDetail:         r.TeacherDetail,
-		CourseName:            r.CourseName,
-		StudentUsername:       r.StudentUsername,
-		StudentDetail:         r.StudentDetail,
-		DefaultFee:            r.DefaultFee,
-		DefaultDurationMinute: r.DefaultDurationMinute,
-	}
+	return GetClassesRow(r)
 }
