dfa3154b17b9cf88c1269434136719fdd92b7e0b
FerdiantJoshua <ferdiant.98@gmail.com>
2023-09-03 06:27:12 +0700

    Improve init_user.sql to support MySQL 8.x better

--- scripts/mysql/init_user.sql
+++ scripts/mysql/init_user.sql
@@ -1,5 +1,12 @@
-ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'p4ssw0rd';
+-- use this if you're accessing the MySQL server using MariaDB
+-- ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'p4ssw0rd';
+ALTER USER 'root'@'%' IDENTIFIED BY 'p4ssw0rd';
 DROP USER IF EXISTS 'sonamusica'@'%';
-CREATE USER 'sonamusica'@'%' IDENTIFIED WITH mysql_native_password BY 'p4ssw0rd';
+-- use this if you're accessing the MySQL server using MariaDB
+-- CREATE USER 'sonamusica'@'%' IDENTIFIED WITH mysql_native_password BY 'p4ssw0rd';
+CREATE USER 'sonamusica'@'%' IDENTIFIED BY 'p4ssw0rd';
 GRANT ALL PRIVILEGES ON sonamusica_administration_backend.* TO 'sonamusica'@'%';
 FLUSH PRIVILEGES;
+
+-- This is to allow any user to create trigger, function, and method
+SET GLOBAL log_bin_trust_function_creators = 1;
